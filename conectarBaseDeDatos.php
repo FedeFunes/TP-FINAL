3df87bb1a8ffb4c8daac95170a88eab7f7d31d3b
blamilla <brian.lamilla@gmail.com>
2014-10-28 01:06:34 -0300

    nuevos cambios
    Cambios en todas las pantalla. Se agrego el menu de arriba de todo y los
    links a cada pagina. Se cambio la extension de HTML a PHP porque sino no
    funcionan los estilos

--- conectarBaseDeDatos.php
+++ conectarBaseDeDatos.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 	session_start();
 			
 	$conexion = mysql_connect ("localhost:3306", "root", "") or die ("No se puede conectar con el servidor");
@@ -11,11 +10,4 @@
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }*/
-=======
-$con=mysqli_connect("localhost","root","","cieloytierra");
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
->>>>>>> 63fb144c10621284d706357b8cf4ff6a6abbaac6
 ?>
