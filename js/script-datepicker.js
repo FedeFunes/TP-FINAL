0980e7448a30aac0037448ad2d392b43e57c8e3b
FefoFunes <federicofunes92@gmail.com>
2014-10-28 15:19:56 -0300

    edición datapicker
    cambie los limites de las fechas

--- js/script-datepicker.js
+++ js/script-datepicker.js
@@ -1,9 +1,9 @@
 $(function() {
-	$( "#fecha_ida" ).datepicker({dateFormat: 'dd/mm/yy', minDate: 2, maxDate: "+1M +20D" });
+	$( "#fecha_ida" ).datepicker({dateFormat: 'dd/mm/yy', minDate: 0, maxDate: "+1M +20D" });
 });
 
 $(function() {
-$( "#fecha_vuelta" ).datepicker({dateFormat: 'dd/mm/yy', minDate: 2, maxDate: "+1M +20D" });
+$( "#fecha_vuelta" ).datepicker({dateFormat: 'dd/mm/yy', minDate: 1, maxDate: "+1M +21D" });
 });
 
 $(function() {
