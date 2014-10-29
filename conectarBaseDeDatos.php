fc51039cb67eb29e161d5961bca80af5216629c3
FefoFunes <federicofunes92@gmail.com>
2014-10-29 12:02:17 -0300

    Borre "mysqi_close($conexion);"
    No debería estar porque conecta y cierra la conexion a la vez

--- conectarBaseDeDatos.php
+++ conectarBaseDeDatos.php
@@ -3,6 +3,4 @@
 			
 	$conexion = mysqli_connect ("localhost:3306", "root") or die ("No se puede conectar con el servidor");
 	mysqli_select_db($conexion,"cieloytierra") or die ("No se puede seleccionar la base de datos");
-	
-	mysqli_close($conexion);
 ?>
