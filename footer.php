9d0075264ab8d6872e6bbe25d6472d09fa52be4c
FefoFunes <federicofunes92@gmail.com>
2014-10-29 15:54:46 -0300

    Separé el nav-bar y el footer en 2 phps
    Para después incluirlos en las páginas con include();

---
+++ footer.php
@@ -0,0 +1,7 @@
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<footer class="footer">
+					<p>Programaci&oacuten Web II - 2°Cuatrimestre 2014</p>
+				</footer>
+			</div><!-- /.col-md-10 col-md-offset-1 -->
+		</div><!-- /.row -->
