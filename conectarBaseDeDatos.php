9152d96aebb6cea3136ce76b0d6ac84757020d7c
FefoFunes <federicofunes92@gmail.com>
2014-10-29 10:17:10 -0300

    Cambié "mysql" por "mysqli"
    "The MySQLi extension is designed to work with MySQL version 4.1.13 or
    newer."  por eso lo hice :P sino no me corre

---
+++ conectarBaseDeDatos.php
@@ -0,0 +1,8 @@
+<?php
+	session_start();
+			
+	$conexion = mysqli_connect ("localhost:3306", "root") or die ("No se puede conectar con el servidor");
+	mysqli_select_db($conexion,"cieloytierra") or die ("No se puede seleccionar la base de datos");
+	
+	mysqli_close($conexion);
+?>
