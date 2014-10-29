fcbdc41da62580565b8af47a1a69c7eb756dcc79
FefoFunes <federicofunes92@gmail.com>
2014-10-29 00:43:12 -0300

    pruebas

--- buscarVuelo.php
+++ buscarVuelo.php
@@ -1,6 +1,10 @@
 <?php
 
-$fecha_ida = $_POST["fecha_ida"];
-echo $fecha_ida;
+$fecha_ida = "10/10/10";
+
+echo substr($fecha_ida,1,4);
+
+$d=mktime(0, 0, 0, 8, 12, 2014);
+echo "Created date is " . date("Y-m-d", $d);
 
 ?>
