709f0746df4fc8f4e6c456b516bf3e3ef9dab815
FefoFunes <federicofunes92@gmail.com>
2014-10-29 16:10:07 -0300

    Incluí footer.php  y nav-bar.php

--- index.php
+++ index.php
@@ -29,43 +29,10 @@
     <![endif]-->
 
 </head>
-<body>
+<body class="body-index">
 	<div class="container">
 		
-		<div class="row">
-				<div class="col-md-10 col-md-offset-1">
-
-					<nav class="navbar navbar-default fixed-top" role="navigation">
-					  	<div class="container-fluid">
-						    <!-- Brand and toggle get grouped for better mobile display -->
-						    <div class="navbar-header">
-						      	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-						        	<span class="sr-only">Toggle navigation</span>
-							        <span class="icon-bar"></span>
-							        <span class="icon-bar"></span>
-							        <span class="icon-bar"></span>
-						     	</button>
-						      	<a class="navbar-brand" href="index.php">
-									<img src="imgs/logo.png" width="40px" height="40px" />
-								</a>
-						    </div>
-
-					    <!-- Collect the nav links, forms, and other content for toggling -->
-					    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						    <ul class="nav nav-pills centrar-navbar">
-					      	 	<li class="active"><a href="index.html">Inicio</a></li>
-						        <li><a href="miReserva.php">Mi Reserva</a></li>
-						        <li><a href="vuelos.php">Vuelos</a></li>
-						        <li><a href="contacto.php">Contacto</a></li>
-						    </ul>
-					        <ul class="nav navbar-nav navbar-right">
-				            	<a href="#"><li><span class="glyphicon glyphicon-user"></span> Administrador</a></li>  
-				            </ul>
-					    </div><!-- /.navbar-collapse -->
-					</div><!-- /.container-fluid -->
-				</nav><!-- /.navbar navbar-default fixed-top -->
-			</div><!-- /.col-md-10 col-md-offset-1 -->
-		</div><!-- /.row -->
+		<?php include("nav-bar.php") ?>
 
 		<!-- Sección Elegir-Vuelo -->
 		<div class="row">
@@ -420,8 +387,10 @@
 			</div>
 			<!-- /.col-md-12 -->
 		</div>
-		<!--/row-->
-		
+		<!-- /.row -->
+	
+		<?php include("footer.php") ?>
+
 	</div> <!-- /.container -->
 
 	<!-- Primero siempre cargo las librerías jQuery -->	
