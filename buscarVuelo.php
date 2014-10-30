d4076e31184ae875a2b6875c080ecaa25c5c959d
FefoFunes <federicofunes92@gmail.com>
2014-10-30 13:50:30 -0300

    comentario

--- buscarVuelo.php
+++ buscarVuelo.php
@@ -13,12 +13,12 @@ $ciudadOrigen = $_POST["ciudadOrigen"];
 $fechaIda = $_POST["fechaIda"];
 $date = date_create_from_format("d-m-Y",$fechaIda); // crea un object datetime de $fecha_ida y le indico el formato de $fecha_ida 
 $diaIda = date_format($date,"l"); // obtiene el nombre del día de la fecha PERO lo obtiene en inglés
-echo $diaIda;
+echo $diaIda; // para que vean
 
 $fechaVuelta = $_POST["fechaVuelta"];
 $date = date_create_from_format("d-m-Y",$fechaVuelta);  
 $diaVuelta = date_format($date,"l");
-echo $diaVuelta;
+echo $diaVuelta; // para que vean
 
 
 ?>
