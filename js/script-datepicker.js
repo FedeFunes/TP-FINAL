29843e79178b81f54da142fbf82cc728cfd17896
FefoFunes <federicofunes92@gmail.com>
2014-10-30 11:09:11 -0300

    "reestructure el codigo"

--- js/script-datepicker.js
+++ js/script-datepicker.js
@@ -1,11 +1,24 @@
 $(function() {
-	$( "#fecha_ida" ).datepicker({dateFormat: 'dd/mm/yy', minDate: 0, maxDate: "+1M +20D" });
+	$( "#fecha_ida" ).datepicker({
+		dateFormat: 'dd-mm-yy', 
+		minDate: 0, 
+		maxDate: "+1M +20D" 
+	});
 });
 
 $(function() {
-$( "#fecha_vuelta" ).datepicker({dateFormat: 'dd/mm/yy', minDate: 1, maxDate: "+1M +21D" });
+$( "#fecha_vuelta" ).datepicker({
+		dateFormat: 'dd-mm-yy', 
+		minDate: 1, 
+		maxDate: "+1M +21D" 
+	});
 });
 
 $(function() {
-	$( "#fecha_nac" ).datepicker({dateFormat: 'dd/mm/yy', changeMonth: true, changeYear: true, yearRange: '-100:+0'});
+	$( "#fecha_nac" ).datepicker({
+		dateFormat: 'dd-mm-yy', 
+		changeMonth: true, 
+		changeYear: true, 
+		yearRange: '-100:+0'
+	});
 });
