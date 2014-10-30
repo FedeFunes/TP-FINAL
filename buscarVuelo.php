d759bcec0f520ae480063c7fb841878b5176f3d8
FefoFunes <federicofunes92@gmail.com>
2014-10-30 12:27:45 -0300

    el php que llama form elegir-vuelo

---
+++ buscarVuelo.php
@@ -0,0 +1,24 @@
+<?php
+
+$tipoViaje = $_POST["tipoViaje"];
+
+$provinciaOrigen = $_POST["provinciaOrigen"];
+
+$ciudadOrigen = $_POST["ciudadOrigen"];
+
+$provinciaDestino = $_POST["provinciaDestino"];
+
+$ciudadOrigen = $_POST["ciudadOrigen"];
+
+$fechaIda = $_POST["fechaIda"];
+$date = date_create_from_format("d-m-Y",$fechaIda); // crea un object datetime de $fecha_ida y le indico el formato de $fecha_ida 
+$diaIda = date_format($date,"l"); // obtiene el nombre del día de la fecha PERO lo obtiene en inglés
+echo $diaIda;
+
+$fechaVuelta = $_POST["fechaVuelta"];
+$date = date_create_from_format("d-m-Y",$fechaVuelta);  
+$diaVuelta = date_format($date,"l");
+echo $diaVuelta;
+
+
+?>
