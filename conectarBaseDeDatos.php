69e66f5cfc5d14fd447e7f6c4273c76ffcf10fc1
FefoFunes <federicofunes92@gmail.com>
2014-11-02 23:46:40 -0300

    le quite el session_star();

--- conectarBaseDeDatos.php
+++ conectarBaseDeDatos.php
@@ -1,6 +1,4 @@
-<?php
-	session_start();
-			
+<?php	
 	$conexion = mysqli_connect ("localhost:3306", "root") or die ("No se puede conectar con el servidor");
 	mysqli_select_db($conexion,"cieloytierra") or die ("No se puede seleccionar la base de datos");
 ?>
