acb24d5183be77eff097f598d33a5cf3d1ebfeb4
FefoFunes <federicofunes92@gmail.com>
2014-11-04 12:23:24 -0300

    actualize reservar.php

--- reservar.php
+++ reservar.php
@@ -24,41 +24,9 @@
     </head>
     <body>
     <div class="container">
-		<div class="row">
-				<div class="col-md-10 col-md-offset-1">
-
-					<nav class="navbar navbar-default fixed-top" role="navigation">
-					  	<div class="container-fluid">
-						    <!-- Brand and toggle get grouped for better mobile display -->
-						    <div class="navbar-header">
-						      	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-						        	<span class="sr-only">Toggle navigation</span>
-							        <span class="icon-bar"></span>
-							        <span class="icon-bar"></span>
-							        <span class="icon-bar"></span>
-						     	</button>
-						      	<a class="navbar-brand" href="index.html">
-									<img src="imgs/logo.png" width="40px" height="40px" />
-								</a>
-						    </div>
-
-					    <!-- Collect the nav links, forms, and other content for toggling -->
-					    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						    <ul class="nav nav-pills">
-					      	 	<li><a href="index.php">Inicio</a></li>
-						        <li class="active"><a href="miReserva.php">Resevas</a></li>
-						        <li><a href="vuelos.php">Vuelos</a></li>
-						        <li><a href="contacto.php">Contacto</a></li>
-						    </ul>
-					        <ul class="nav navbar-nav navbar-right">
-				            	<a href="#"><li><span class="glyphicon glyphicon-user"></span> Administrador</a></li>  
-				            </ul>
-					    </div><!-- /.navbar-collapse -->
-					</div><!-- /.container-fluid -->
-				</nav><!-- /.navbar navbar-default fixed-top -->
-			</div><!-- /.col-md-10 col-md-offset-1 -->
-		</div><!-- /.row -->
 		
+        <?php include("nav-bar.php");?>
+
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 
@@ -136,6 +104,9 @@
                 </form>
             </div>
         </div>        
+    
+        <?php include("footer.php");?>
+
     </div>
     
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
