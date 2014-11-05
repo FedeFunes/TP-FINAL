7426e275cb2b94e8c64aa1ec0c9df6658acdfc18
blamilla <brian.lamilla@gmail.com>
2014-11-05 00:36:59 -0300

    Creacion pantalla Programacion de Vuelos
    Cree la pantalla de programacion de vuelos ahora si entran pueden ver la
    tabla con los vuelos disponibles. A eso habria que agregarle estilos y
    ver la manera de solucionar el tema de los acentos y ñ que se ven mal.
    Lo googlee y es un tema del charset metiendo un tag meta con el charset
    que usa no lo pude solucionar tampoco.
    Adicionalmente, habria que mejorar el código para que en la columna Dias
    vuelos muestre los dias en que viaja cada vuelo programado ya que no
    encontre la manera de hacerlo

--- grilla-vuelos.php
+++ grilla-vuelos.php
@@ -2,21 +2,143 @@
 <html lang="en">
     <head>
     <meta charset="utf-8">
+	<meta http-equiv="conten-type" content="text/html; charset=UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CYT Aerol&iacuteneas - Reservar</title>
+    <title>CYT Aerol&iacuteneas - Programaci&oacute;n de Vuelos</title>
 
     <?php include("librerias-css.php"); ?>
     
     </head>
     <body>
-    <div class="container">
+		<?php 
+		include("conectarBaseDeDatos.php");
+		//session_start();
+		?>
+		   
+		<div class="container">
+			<?php include("nav-bar.php") ?>
+			<?php include("librerias-js.php"); ?>
 
-        <?php include("nav-bar.php") ?>
+			<div class="jumbotron">
+				<h1>
+					Programaci&oacute;n de Vuelos
+				</h1>
+			</div>
+					
+					
+		<div class="table-responsive">  
+			<table class="table" id="tabla">
+				<thead>
+				<div class="panel panel primary">
+				  <tr>
+					<th>
+					  Id
+					</th>
+									
+					
+					<th>
+					  Aeropuerto Origen
+					</th>
+									
+					
+					<th>
+					  Ciudad Origen
+					</th>
+									
+					
+					<th>
+					  Aeropuerto Destino
+					</th>
+									
+					
+					<th>
+					  Ciudad Destino
+					</th>
+					
+					<th>
+					  Precio Primera
+					</th>
+					
+					<th>
+					  Precio Economy
+					</th>
+					
+					<th>
+					  Vuelos D&iacute;as
+					</th>
+								  
+				  </tr>
+				</div>			  
+				</thead>
+							
 
-        <?php include("footer.php") ?>
-    
-    <?php include("librerias-js.php"); ?>
-   
-    </body>
+				<tbody>
+				  <?php  
+							
+							// Creamos la tabla con los campos que queremos mostrar de las personas
+							// Iteramos a través de los resultados que nos devolvió la consulta
+							// Le colocamos un id a cada fila para cuando las vayamos a eliminar con jQuery
+							// La acción de eliminar la llamamos a través de un evento onclick e indicando el id a eliminar
+							$query = "SELECT PV.idProgramacionVuelo, A1.descripcion as aeropuertoOrigen, C1.descripcion as ciudadOrigen, A2.descripcion as aeropuertoDestino, 
+										C2.descripcion as ciudadDestino, PV.precio_primera as precioPrimera, PV.precio_economy as precioEconomy
+										FROM programacionvuelos PV JOIN 
+											aeropuertos A1 ON A1.idAeropuerto = PV.cod_aeropuerto_origen JOIN 
+											aeropuertos A2 ON A2.idAeropuerto = PV.cod_aeropuerto_destino JOIN 
+											ciudades C1 ON C1.idCiudad = A1.cod_ciudad JOIN 
+											ciudades C2 ON C2.idCiudad = A2.cod_ciudad JOIN 
+											provincias P1 ON C1.cod_provincia = P1.idProvincia JOIN
+											provincias P2 ON C2.cod_provincia = P2.idProvincia
+										ORDER BY PV.idProgramacionVuelo;";
+							$result = mysqli_query($conexion,$query);
+							while($fila = $result->fetch_assoc()){
+										echo
+										'
+				  
+				  <tr id="fila_'.$fila['idProgramacionVuelo'].'">
+					<td>
+					  '.$fila['idProgramacionVuelo'].'
+					</td>
+											
+					
+					<td>
+					  '.$fila['aeropuertoOrigen'].'
+					</td>
+											
+					
+					<td>
+					  '.$fila['ciudadOrigen'].'
+					</td>
+											
+					
+					<td>
+					  '.$fila['aeropuertoDestino'].'
+					</td>                        
+					
+					<td>
+					  '.$fila['ciudadDestino'].'
+					</td>
+					
+					<td>
+					  '.$fila['precioPrimera'].'
+					</td>
+					
+					<td>
+					  '.$fila['precioEconomy'].'
+					</td>
+										  
+				  </tr>';
+								  } 
+							  ?>
+							  
+				</tbody>
+						  
+			</table>
+		</div>
+		
+		<?php include("footer.php") ?>  
+				  
+		</div>
+	</body>
 </html>
