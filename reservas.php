89b4d43e48e2fab655b6198bc6fc47e9be443dc4
FefoFunes <federicofunes92@gmail.com>
2014-11-05 22:37:34 -0300

    pagina reservas

---
+++ reservas.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CYT Aerol&iacuteneas - Reservar</title>
+
+    <?php include("librerias-css.php"); ?>
+    
+    </head>
+    <body>
+    <div class="container">
+
+        <?php include("nav-bar.php") ?>
+
+        <row>
+            <div class="col-md-6 col-md-offset-3">
+                <div class="panel panel-primary">
+                     <div class="panel-body">   
+                        
+                        <!-- Form - Mi Reserva -->
+                        <form role="form" method="post" action="buscarReservar.php">
+                            
+                            <!-- Ingrese Nro de Reserva -->
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Nro de Reserva">
+                                <span class="help-block" style="color:black;">Consulte y realice el Pago o Check-In de su Reserva</span>
+                            </div>
+                            
+                            <!-- Botón "Buscar" --> 
+                            <button type="submit" class="btn btn-primary">Buscar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </row>
+
+        <?php include("footer.php") ?>
+    
+        <?php include("librerias-js.php"); ?>
+   
+    </body>
+</html>
