90e38a4d1d339a5be3059953cc60fd70e8884ca3
FefoFunes <federicofunes92@gmail.com>
2014-11-05 22:38:34 -0300

    template para crear nuevas paginas

---
+++ template.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CYT Aerol&iacuteneas - Reservar</title>
+
+    <?php include("librerias-css.php"); ?>
+    
+    </head>
+    <body>
+    <div class="container">
+
+        <?php include("nav-bar.php") ?>
+
+        <?php include("footer.php") ?>
+    
+        <?php include("librerias-js.php"); ?>
+   
+    </body>
+</html>
