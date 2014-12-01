fd30689112d837ac842d467504275a01c9ade80a
fefofunes <federicofunes92@gmail.com>
2014-11-30 22:06:04 -0300

    pdf realizar reserva

--- resultadoReservarVuelos.php
+++ resultadoReservarVuelos.php
@@ -10,11 +10,9 @@ $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,10,"RESERVA",0,1);
 $pdf->Cell(40,10,"Nro de Reserva - Ida: ".$_SESSION["idReservaIda"]."",0,1);
 $pdf->Cell(40,10,"Nro de Reserva - Vuelta: ".$_SESSION["idReservaVuelta"]."",0,1);
-$pdf->Cell(40,10,"Fecha Vuelo: ".$_SESSION["fechaVuelo"]."",0,1);
 $pdf->Cell(40,10,"Origen: ".$_SESSION["provinciaOrigen"]." - ".$_SESSION["ciudadOrigen"]."",0,1);  
 $pdf->Cell(40,10,"Origen: ".$_SESSION["provinciaDestino"]." - ".$_SESSION["ciudadDestino"]."",0,1);
 $pdf->Cell(40,10,"Categoria: ".$_SESSION["categoria"]."",0,1);
-$pdf->Cell(40,10,"Precio: ".$_SESSION["precioCategoria"]."",0,1);
 $pdf->Output();
 
 ?>
