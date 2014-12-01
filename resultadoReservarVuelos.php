e6ed3a67e4e68e1c0af22af69e65f621d3451f05
fefofunes <federicofunes92@gmail.com>
2014-12-01 16:26:28 -0300

    actualizcion

--- resultadoReservarVuelos.php
+++ resultadoReservarVuelos.php
@@ -11,7 +11,7 @@ $pdf->Cell(40,10,"RESERVA",0,1);
 $pdf->Cell(40,10,"Nro de Reserva - Ida: ".$_SESSION["idReservaIda"]."",0,1);
 $pdf->Cell(40,10,"Nro de Reserva - Vuelta: ".$_SESSION["idReservaVuelta"]."",0,1);
 $pdf->Cell(40,10,"Origen: ".$_SESSION["provinciaOrigen"]." - ".$_SESSION["ciudadOrigen"]."",0,1);  
-$pdf->Cell(40,10,"Origen: ".$_SESSION["provinciaDestino"]." - ".$_SESSION["ciudadDestino"]."",0,1);
+$pdf->Cell(40,10,"Destino: ".$_SESSION["provinciaDestino"]." - ".$_SESSION["ciudadDestino"]."",0,1);
 $pdf->Cell(40,10,"Categoria: ".$_SESSION["categoria"]."",0,1);
 $pdf->Output();
 
